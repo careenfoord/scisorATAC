9f8863237dd9277b66042d0310f2742b7547c4ce
careenfoord <131822458+careenfoord@users.noreply.github.com>
2023-05-01 11:29:42 -0400

    Update DownloadReferenceFiles.sh

--- inst/bash/DownloadReferenceFiles.sh
+++ inst/bash/DownloadReferenceFiles.sh
@@ -4,6 +4,6 @@
 mkdir Refs
 cd Refs
 
-curl -L -o out.zip "https://uc3afb8b11bb428b86566fecfe78.dl.dropboxusercontent.com/zip_download_get/BekclaPzvbDiwt0NAE2msOg1J8MecL5tDhpIxE1VkQoTrs_8c2OYqslExrfV0iMocR7aBcV-QSgTleOJvO0i6KOPJ966QtpukjE_dFV3txp8Fw?_download_id=65037909902751562821203991441972571281491068956593584015480496&_notify_domain=www.dropbox.com&dl=1"
+curl -L -o out.zip "https://www.dropbox.com/sh/y5ly6bswf4qqrg9/AACWrqJ74AN8RRPLVD9xe38Fa?dl=1"
 
 unzip out.zip
